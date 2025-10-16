7e9cb4064c54151c06abe57ff4118e0b07b95c15
alanabrahampkochumon-zqict <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2025-10-16 11:51:07 +0530

    Add checks to ensure the datapoints near 1km and high duration are not skewing the data

--- Analysis.R
+++ Analysis.R
@@ -13,8 +13,9 @@ plot_directory <- "Plots/"
 
 # Load the cleaned dataset into memory
 data_frame <- read.csv("./Data/Cleaned_Data_14_10_2025.csv")
+data_frame <- data_frame |> select(-c("X"))
 
-# NOTE: This may not work in Dataspell (No graph is plotted),
+# NOTE: geom_point work in Dataspell (No graph is plotted),
 # But it will work in RStudio
 # You can export the graph as png though
 plot <- ggplot(data_frame, aes(x = distance_rode_km, y = ride_length)) +
@@ -25,6 +26,14 @@ plot <- ggplot(data_frame, aes(x = distance_rode_km, y = ride_length)) +
     y = "Ride Length (minutes)",
     title = "Ride Length vs Distance - Outlier Check"
   )
+# Lots of clustering near zero km mark
+
+data_frame |>
+  filter(distance_rode_km < 1 & ride_length > 60) |>
+  nrow()
+# Rides lengths that are less than 1km in distance and 60 mins in ride
+# around 25k which is only 0.43% of the entire datasets
+# so not removing it since, it wont drastically change the data analysis outcome
 
 # NOTE: Can't load the graph in DataSpell so exporting it to png
 ggsave(
