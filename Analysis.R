16602c861da8de31d8b9df84ba4cd2db662c2db8
alanabrahampkochumon-zqict <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2025-10-16 11:34:08 +0530

    Start Analysis

---
+++ Analysis.R
@@ -0,0 +1,36 @@
+#' ### ANALYSIS ###
+### HOUSEKEEPING ###
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("ggrastr")
+
+# Load the libraries required for analysis and data importing
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+plot_directory <- "Plots/"
+
+# Load the cleaned dataset into memory
+data_frame <- read.csv("./Data/Cleaned_Data_14_10_2025.csv")
+
+# NOTE: This may not work in Dataspell (No graph is plotted),
+# But it will work in RStudio
+# You can export the graph as png though
+plot <- ggplot(data_frame, aes(x = distance_rode_km, y = ride_length)) +
+  geom_hex() +
+  scale_y_log10() +
+  labs(
+    x = "Distance (KM)",
+    y = "Ride Length (minutes)",
+    title = "Ride Length vs Distance - Outlier Check"
+  )
+
+# NOTE: Can't load the graph in DataSpell so exporting it to png
+ggsave(
+  paste(plot_directory, "ride_length_vs_distance.png"),
+  plot,
+  width = 8,
+  height = 6,
+  dpi = 150
+)
