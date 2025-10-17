be72fe04316421dac99585f338e4eac064cbb764
alanabrahampkochumon-zqict <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2025-10-17 10:27:17 +0530

    Start Analysis: Insufficient memory

--- Analysis.R
+++ Analysis.R
@@ -27,6 +27,14 @@ plot <- ggplot(data_frame, aes(x = distance_rode_km, y = ride_length)) +
     title = "Ride Length vs Distance - Outlier Check"
   )
 # Lots of clustering near zero km mark
+# NOTE: Can't load the graph in DataSpell so exporting it to png
+ggsave(
+  paste(plot_directory, "ride_length_vs_distance.png"),
+  plot,
+  width = 8,
+  height = 6,
+  dpi = 150
+)
 
 data_frame |>
   filter(distance_rode_km < 1 & ride_length > 60) |>
@@ -35,9 +43,33 @@ data_frame |>
 # around 25k which is only 0.43% of the entire datasets
 # so not removing it since, it wont drastically change the data analysis outcome
 
-# NOTE: Can't load the graph in DataSpell so exporting it to png
+#' ### BEGIN Analysis ###
+# Let's start by analysing the mean and median length of ride for
+# Casual vs members
+ride_summary <- data_frame |>
+  group_by(member_casual) |>
+  summarise(
+    mean_distance = mean(distance_rode_km),
+    median_distance = median(distance_rode_km),
+    mean_duration = mean(ride_length),
+    median_duration = median(ride_length),
+    total_rides = n(),
+  )
+# The distance is around the same, but casual rides tends to be more longer
+# with more riders being members
+
+# Lets see the distribution in a chart
+plot <- data_frame |>
+  filter(member_casual == "casual") |>
+  ggplot(aes(x = ride_length_cat)) +
+  geom_bar() +
+  labs(
+    title = "Ride Duration vs Type of Rider",
+    x = "Ride Duration",
+    y = "Member vs Casual"
+  )
 ggsave(
-  paste(plot_directory, "ride_length_vs_distance.png"),
+  paste(plot_directory, "ride_duration_vs_casual_member.png"),
   plot,
   width = 8,
   height = 6,
